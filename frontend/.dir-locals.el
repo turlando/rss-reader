a75805beedf2fe299427cea2dd80a58e23fe6dc4
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-01-20 16:41:29 +0100

    Import local emacs config for frontend

---
+++ frontend/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; disable .#FILE because react build system trips on it very badly
+((nil . ((create-lockfiles . nil)
+         (make-backup-files . nil))))
