d8a97d7e491c98b88752ec12007531dd81729dd1
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-02-01 11:22:12 +0100

    From scratch

--- frontend/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-;; disable .#FILE because react build system trips on it very badly
-((nil . ((create-lockfiles . nil)
-         (make-backup-files . nil))))
