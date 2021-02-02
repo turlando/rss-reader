5e6bab26044a9d3430f8646f14667169d0a9c3b0
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-02-02 04:14:47 +0100

    Fix onClick callback

--- frontend/src/components/tree/Tree.tsx
+++ frontend/src/components/tree/Tree.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import {SubscriptionTree, SubscriptionTreeNode, SubscriptionTreeNodeType} from '../../api';
+import {
+    SubscriptionTree, SubscriptionTreeNode, SubscriptionTreeNodeType,
+    Folder, Feed
+} from '../../api';
 
 import FolderNode from './FolderNode';
 import FeedNode from './FeedNode';
@@ -8,12 +11,22 @@ import FeedNode from './FeedNode';
 import './Tree.css';
 
 
+type OnClickEvent = React.MouseEvent<HTMLDivElement, MouseEvent>;
+
 interface Props {
     tree: SubscriptionTree;
+    onFolderClick?: (evt: OnClickEvent, folder: Folder) => void;
+    onFeedClick?: (evt: OnClickEvent, feed: Feed) => void;
 }
 
 
-const Tree: React.FC<Props> = ({tree}) => {
+const Tree: React.FC<Props> = ({
+    tree,
+    onFolderClick = (evt: OnClickEvent, folder: Folder) => null,
+    onFeedClick = (evt: OnClickEvent, feed: Feed) => null
+}) => {
+    const makeNode = nodeMaker(onFolderClick, onFeedClick);
+
     return (
         <div className="Tree">
             {tree.map(node => makeNode(node))}
@@ -22,19 +35,46 @@ const Tree: React.FC<Props> = ({tree}) => {
 };
 
 
-const makeNode = (node: SubscriptionTreeNode) => {
-    if (node.type === SubscriptionTreeNodeType.Folder && node.children.length === 0)
-        return <FolderNode key={`folder-${node.id}`} folder={node}/>;
-
-    if (node.type === SubscriptionTreeNodeType.Folder && node.children.length !== 0)
+const nodeMaker = (
+    onFolderClick: (evt: OnClickEvent, folder: Folder) => void,
+    onFeedClick: (evt: OnClickEvent, feed: Feed) => void
+) => function fn(node: SubscriptionTreeNode) {
+    if (
+        node.type === SubscriptionTreeNodeType.Folder
+     && node.children.length === 0
+    ) {
         return (
-            <FolderNode key={`folder-${node.id}`} folder={node}>
-                {node.children.map(child => makeNode(child))}
+            <FolderNode
+                key={`folder-${node.id}`}
+                folder={node}
+                onClick={evt => onFolderClick(evt, node)}
+            />
+        );
+    }
+
+    if (
+        node.type === SubscriptionTreeNodeType.Folder
+     && node.children.length !== 0
+    ) {
+        return (
+            <FolderNode
+                key={`folder-${node.id}`}
+                folder={node}
+                onClick={evt => onFolderClick(evt, node)}
+            >
+                {node.children.map(child => fn(child))}
             </FolderNode>
         );
+    }
 
     if (node.type === SubscriptionTreeNodeType.Feed)
-        return <FeedNode key={`feed-${node.id}`} feed={node}/>;
+        return (
+            <FeedNode
+                key={`feed-${node.id}`}
+                feed={node}
+                onClick={evt => onFeedClick(evt, node)}
+            />
+        );
 };
 
 
